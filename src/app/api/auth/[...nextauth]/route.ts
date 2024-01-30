7d23d913401f3585a1534d7e737b0f9a1baaaa53
rp0463 <rajanpokharel@my.unt.edu>
2024-01-30 13:10:18 -0600

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,4 @@
+import { mergeAnonToUser } from "@/library/database/cart";
 import { prisma } from "@/library/database/prisma";
 import { env } from "@/library/env";
 import { PrismaAdapter } from "@auth/prisma-adapter";
@@ -5,50 +6,27 @@ import { NextAuthOptions } from "next-auth";
 import { Adapter } from "next-auth/adapters";
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
-import CredentialsProvider from "next-auth/providers/credentials";
-import EmailProvider from 'next-auth/providers/email';
-import bcrypt from "bcrypt";
 
 export const authOpt: NextAuthOptions = {
-	adapter: PrismaAdapter(prisma) as Adapter,
-	providers: [
-    	GoogleProvider({
-        	clientId: env.GOOGLE_CLIENT_ID,
-        	clientSecret: env.GOOGLE_CLIENT_SECRET
-    	}),
-    	CredentialsProvider({
-        	name: "Credentials",
-        	credentials: {
-            	email: { label: "email", type: "text", placeholder: "x@example.com" },
-            	password: { label: "Password", type: "password" }
-        	},
-        	async authorize(credentials, req) {
-            	if (!credentials || !credentials.email || !credentials.password) {
-                	return null;
-            	}
-
-            	const user= await prisma.user.findUnique({
-                	where: { email: credentials.email }
-            	});
-            	if (!user) {
-                	//console.log("WORKS");
-                	return null
-            	}
-              const passwordsMatch = await bcrypt.compare(credentials.password, user.hashedPassword);
-              if(!passwordsMatch){
-                return null;
-              }
-              else{
-                return user;
-              }
-
-        	}
-    	}),
-	],
-	session: {
-    strategy: "jwt"
-  },
-  secret: env.NEXTAUTH_SECRET
+    adapter: PrismaAdapter(prisma) as Adapter,
+    providers: [
+        GoogleProvider({
+            clientId: env.GOOGLE_CLIENT_ID,
+            clientSecret: env.GOOGLE_CLIENT_SECRET
+        })
+    ],
+    callbacks: {
+        session({session, user}) {
+            session.user.id = user.id
+            return session;
+        },
+    },
+    events: {
+        async signIn({user}) {
+            await mergeAnonToUser(user.id);
+        }
+    }
 }
+
 const handler = NextAuth(authOpt);
 export { handler as GET, handler as POST }
