460dbea6aff455b61f0712d63c7cd063b9e04570
rp0463 <rajanpokharel@my.unt.edu>
2024-01-29 20:52:55 -0600

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -6,9 +6,6 @@ import { NextAuthOptions } from "next-auth";
 import { Adapter } from "next-auth/adapters";
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
-import CredentialsProvider from "next-auth/providers/credentials";
-import EmailProvider from 'next-auth/providers/email';
-import bcrypt from "bcrypt";
 
 export const authOpt: NextAuthOptions = {
     adapter: PrismaAdapter(prisma) as Adapter,
@@ -16,42 +13,8 @@ export const authOpt: NextAuthOptions = {
         GoogleProvider({
             clientId: env.GOOGLE_CLIENT_ID,
             clientSecret: env.GOOGLE_CLIENT_SECRET
-        }),
-        CredentialsProvider({
-        	name: "Credentials",
-        	credentials: {
-            	email: { label: "email", type: "text", placeholder: "x@example.com" },
-            	password: { label: "Password", type: "password" }
-        	},
-        	async authorize(credentials, req) {
-            	if (!credentials || !credentials.email || !credentials.password) {
-                	return null;
-            	}
-
-            	const user= await prisma.user.findUnique({
-                	where: { email: credentials.email }
-            	});
-            	if (!user) {
-                	//console.log("WORKS");
-                	return null
-            	}
-              const passwordsMatch = await bcrypt.compare(credentials.password, user.hashedPassword);
-              if(!passwordsMatch){
-                return null;
-              }
-              else{
-                return user;
-              }
-
-        	}
-    	}),
-	],
-	session: {
-    strategy: "jwt"
-  },
-
-    secret: env.NEXTAUTH_SECRET,
-
+        })
+    ],
     callbacks: {
         session({session, user}) {
             session.user.id = user.id
@@ -64,7 +27,6 @@ export const authOpt: NextAuthOptions = {
         }
     }
 }
-
 
 const handler = NextAuth(authOpt);
 export { handler as GET, handler as POST }
