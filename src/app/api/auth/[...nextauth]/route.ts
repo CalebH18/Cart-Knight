39d6bfbc9a2e3848ed0b896b6681a9760a2a5521
rp0463 <rajanpokharel@my.unt.edu>
2024-01-29 20:30:46 -0600

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -26,7 +26,45 @@ export const authOpt: NextAuthOptions = {
             await mergeAnonToUser(user.id);
         }
     }
+},
+CredentialsProvider({
+        	name: "Credentials",
+        	credentials: {
+            	email: { label: "email", type: "text", placeholder: "x@example.com" },
+            	password: { label: "Password", type: "password" }
+        	},
+        	async authorize(credentials, req) {
+            	if (!credentials || !credentials.email || !credentials.password) {
+                	return null;
+            	}
+
+            	const user= await prisma.user.findUnique({
+                	where: { email: credentials.email }
+            	});
+            	if (!user) {
+                	//console.log("WORKS");
+                	return null
+            	}
+              const passwordsMatch = await bcrypt.compare(credentials.password, user.hashedPassword);
+              if(!passwordsMatch){
+                return null;
+              }
+              else{
+                return user;
+              }
+
+        	}
+    	}),
+	],
+	session: {
+    strategy: "jwt"
+  },
+secret: env.NEXTAUTH_SECRET
 }
+
 
 const handler = NextAuth(authOpt);
 export { handler as GET, handler as POST }
+
+
+
