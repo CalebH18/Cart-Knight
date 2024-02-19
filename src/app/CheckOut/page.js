3518081899e78631bf084ba4bbe6b21b1aa88871
Caleb Heller <114450914+CalebH18@users.noreply.github.com>
2024-02-18 19:50:33 -0600

    Add files via upload
    Adding the Checkout page

---
+++ src/app/CheckOut/page.js
@@ -0,0 +1,40 @@
+"use client";
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+//import { env } from "@/library/env";
+
+function CheckOut() {
+  return (
+    <div className="h-screen bg-slate-900 flex items-center justify-center">
+      <PayPalScriptProvider
+        options={{
+          clientId:
+          "INSERT_CLIENT_ID",
+        }}
+      >
+        <PayPalButtons
+          style={{ layout: "vertical", color: "silver" }}
+          createOrder={async (data, actions) => {
+            const res = await fetch("/api/checkout", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+            });
+            const order = await res.json();
+            console.log(order);
+            return order.id;
+          }}
+          onCancel={(data) => {
+            console.log("Cancelled:", data);
+          }}
+          onApprove={(data, actions) => {
+            console.log("Approved:", data);
+            actions.order.capture();
+          }}
+        />
+      </PayPalScriptProvider>
+    </div>
+  );
+}
+
+export default CheckOut;
