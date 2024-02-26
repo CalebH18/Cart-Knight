17e69e4b57a186fe944e15bb21323e2116a465aa
Caleb Heller <114450914+CalebH18@users.noreply.github.com>
2024-02-25 21:27:46 -0600

    Update env.ts
    Removed Paypal env variables.

--- src/library/env.ts
+++ src/library/env.ts
@@ -5,7 +5,5 @@ const envSCHEMA = zod.object({
     GOOGLE_CLIENT_SECRET: zod.string().nonempty(),
     NEXTAUTH_URL: zod.string().nonempty(),
     NEXTAUTH_SECRET: zod.string().nonempty(),
-    PAYPAL_CLIENT_ID: zod.string().nonempty(),
-    PAYPAL_CLIENT_SECRET: zod.string().nonempty(),
 });
 export const env = envSCHEMA.parse(process.env);
