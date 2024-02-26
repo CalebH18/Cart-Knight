f66c2d052dc4733392c3b06cc882bc84262ba961
Caleb Heller <114450914+CalebH18@users.noreply.github.com>
2024-02-25 21:30:22 -0600

    Update route.js
    Removed NEXT_PUBLIC_ from Secret

--- src/app/api/checkout/route.js
+++ src/app/api/checkout/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import paypal from "@paypal/checkout-server-sdk";
 import { getCart } from "@/library/database/cart";
 
-let environment = new paypal.core.SandboxEnvironment(process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID, process.env.NEXT_PUBLIC_PAYPAL_CLIENT_SECRET);
+let environment = new paypal.core.SandboxEnvironment(process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID, process.env.PAYPAL_CLIENT_SECRET);
 let client = new paypal.core.PayPalHttpClient(environment);
 
 export async function POST() {
