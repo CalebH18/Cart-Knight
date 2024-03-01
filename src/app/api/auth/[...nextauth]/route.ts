37e9ba111abf86ed04fdb7adbd2d8429f0b4b506
jgbridges365 <148490820+jgbridges365@users.noreply.github.com>
2024-03-01 13:54:39 -0600

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,32 +1,5 @@
-import { mergeAnonToUser } from "@/library/database/cart";
-import { prisma } from "@/library/database/prisma";
-import { env } from "@/library/env";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { NextAuthOptions } from "next-auth";
-import { Adapter } from "next-auth/adapters";
 import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/google";
+import authOptions from "./options";
 
-export const authOpt: NextAuthOptions = {
-    adapter: PrismaAdapter(prisma) as Adapter,
-    providers: [
-        GoogleProvider({
-            clientId: env.GOOGLE_CLIENT_ID,
-            clientSecret: env.GOOGLE_CLIENT_SECRET
-        })
-    ],
-    callbacks: {
-        session({session, user}) {
-            session.user.id = user.id
-            return session;
-        },
-    },
-    events: {
-        async signIn({user}) {
-            await mergeAnonToUser(user.id);
-        }
-    }
-}
-
-const handler = NextAuth(authOpt);
+const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST }
